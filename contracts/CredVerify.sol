1b108e374e9b73586cbcc98454e240b1ffe1e9c3
abidoye korede <korexcoded@gmail.com>
2025-04-26 20:31:11 +0100

    credify.sol and .ts file

---
+++ contracts/CredVerify.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract CredVerify is Ownable {
+    IERC20 public stablecoin; // Stablecoin for collateral and payments 
+    uint256 public constant LOAN_DURATION = 12; // 12 months
+    uint256 public constant APR = 8; // 8% annual interest rate
+   
+}

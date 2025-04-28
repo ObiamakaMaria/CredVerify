963761af9d8553c4e76641cf4b765898268bbf7f
DevBigEazi <adesholatajudeen1@gmail.com>
2025-04-28 07:49:15 +0100

    chore: update the solidity compiler version to 0.8.26

--- contracts/CredVerify.sol
+++ contracts/CredVerify.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ReputationNFT.sol";
 
-
-contract CredVerify is Ownable {
+contract CredVerify  {    
     IERC20 public stablecoin; // Stablecoin for collateral and payments 
     uint256 public constant LOAN_DURATION = 12; // 12 months
     uint256 public constant APR = 8; // 8% annual interest rate
-   
 
-
-   
 }
